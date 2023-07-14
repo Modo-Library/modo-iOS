a93f028e3d3f1b74ce3e65f675ea8985a8095823
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-07-14 17:31:53 +0900

    [feat] 카카오로그인 및 구글로그인 연동을 위한 앱파일 설정

--- NewModo/NewModo/NewModoApp.swift
+++ NewModo/NewModo/NewModoApp.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
+import GoogleSignIn
 
 @main
 struct NewModoApp: App {
+    @StateObject var authVM : AuthViewModel = AuthViewModel()
+    
+    init() {
+        // Kakao SDK 초기화
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+    }
+    
     var body: some Scene {
         WindowGroup {
             AuthView()
+                .environmentObject(authVM)
+            
+                .onOpenURL { url in
+                    //카카오
+                    if (AuthApi.isKakaoTalkLoginUrl(url)){
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                    
+                    // 구글
+                    GIDSignIn.sharedInstance.handle(url)
+                }
+                .onAppear {
+                    GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+                        // Check if `user` exists; otherwise, do something with `error`
+                    }
+                }
         }
     }
 }
